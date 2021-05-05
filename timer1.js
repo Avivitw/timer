8567329fe0622fc4e2827fad85ef124270ba00f2
Avivit Weissman <wavivit@gmail.com>
2021-05-05 02:13:06 +0000

    added checks for edge cases

--- timer1.js
+++ timer1.js
@@ -3,9 +3,14 @@ console.log('arrArgs', arrArgs);
 
 const timer = function(array) {
   for (let i = 0; i < array.length; i++) {
-    setTimeout(() => {
-      process.stdout.write('\x07');
-    }, array[i] * 1000);
+    //check if the item is a number through isNan and check that it's not a negative number
+    if ( (!isNaN(array[i])) && array[i] > 0) {
+      setTimeout(() => {
+        process.stdout.write('\x07');
+      }, array[i] * 1000);
+
+    }
+
   }
 };
 
